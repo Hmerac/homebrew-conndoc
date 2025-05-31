16044e118177c18685068393b64a8733ac116f4e
Mert Açıkportalı <mertacikportali@gmail.com>
2025-06-01 01:28:10 +0200

    Update conndoc.rb

--- Formula/conndoc.rb
+++ Formula/conndoc.rb
@@ -3,8 +3,8 @@ class Conndoc < Formula
   
     desc "Command-line tool for diagnosing network connectivity"
     homepage "https://github.com/mertacikportali/conndoc"
-    url "https://files.pythonhosted.org/packages/e9/3a/29bf8279df3de1db684d1554d06f8b6a07c40bbe5e1286408b5d4faf4d94/conndoc-0.1.0.tar.gz"
-    sha256 "d03db0e059c3b147b7bc2fa3990595f1d5db27ac0d6df2c5da5341420306210c"
+    url "https://files.pythonhosted.org/packages/7c/2b/287149a75b799d2a8f3f90cdd11d754b16b8cdcf15699013b3c2981f8bb8/conndoc-0.2.0.tar.gz"
+    sha256 "1e3976731f3cd61f77d6c9d1763ccf648af43ee6a3a5a2671c56103bd29b4f13"
     license "Apache-2.0"
   
     depends_on "python@3.11"
